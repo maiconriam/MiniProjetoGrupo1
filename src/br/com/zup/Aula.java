04ad7e78858aa7287b153508d6bf824eca5c8f37
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-30 15:03:02 -0300

    Criar Atributos Classe Aula

--- src/br/com/zup/Aula.java
+++ src/br/com/zup/Aula.java
@@ -1,4 +1,10 @@
 package br.com.zup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Aula {
+    private List<Horario> horario = new ArrayList<>();
+    private String tipoDaAula;
+    private double valorDaAula;
 }
