2d4490893ed7cacd5835548d6407b81bbf787f71
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-09-30 16:11:16 -0300

    Criar metodo Scanner no Sistema

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -1,4 +1,11 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Sistema {
+
+    public static Scanner capturarDados(String mensagem) {
+        System.out.println(mensagem);
+        return new Scanner(System.in);
+    }
 }
