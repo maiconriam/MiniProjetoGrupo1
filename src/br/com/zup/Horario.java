d55b032d772d0a65139fe4ab397c2e390e256ebf
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-30 15:27:08 -0300

    Criar metodo construtor classe Horario

--- src/br/com/zup/Horario.java
+++ src/br/com/zup/Horario.java
@@ -2,4 +2,11 @@ package br.com.zup;
 
 public class Horario {
     private int horario;
+
+    public Horario() {
+    }
+
+    public Horario(int horario) {
+        this.horario = horario;
+    }
 }
