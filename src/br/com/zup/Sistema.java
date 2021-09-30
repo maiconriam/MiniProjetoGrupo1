8090645dc599724be02ff4ff5a2941eabb5931b4
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-30 16:31:41 -0300

    Criar Metodo cadastrarAluno()

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -1,4 +1,30 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Sistema {
+
+    public static Scanner capturarDados(String mensagem) {
+        System.out.println(mensagem);
+        return new Scanner(System.in);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+    public static Aluno cadastrarAlunos() {
+        String aluno = capturarDados("Digite o nome do aluno que deseja matricular!").nextLine();
+
+        Aluno novoAluno = new Aluno();
+        novoAluno.setAluno(aluno);
+        return novoAluno;
+    }
