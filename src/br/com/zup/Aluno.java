3699ed29642a0a975f51961389cf8c1b7dcafeaf
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-09-30 16:03:21 -0300

    Criar métodos construtores da classe Aluno

--- src/br/com/zup/Aluno.java
+++ src/br/com/zup/Aluno.java
@@ -3,4 +3,28 @@ package br.com.zup;
 public class Aluno {
     private double alturaAluno;
     private double pesoAluno;
+
+    public Aluno(){
+    }
+
+    public Aluno(double alturaAluno, double pesoAluno) {
+        this.alturaAluno = alturaAluno;
+        this.pesoAluno = pesoAluno;
+    }
+
+    public double getAlturaAluno() {
+        return alturaAluno;
+    }
+
+    public void setAlturaAluno(double alturaAluno) {
+        this.alturaAluno = alturaAluno;
+    }
+
+    public double getPesoAluno() {
+        return pesoAluno;
+    }
+
+    public void setPesoAluno(double pesoAluno) {
+        this.pesoAluno = pesoAluno;
+    }
 }
