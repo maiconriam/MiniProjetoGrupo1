35405d1e93ea1201c28c865aa2fed9eda1275904
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-09-30 15:09:11 -0300

    Criar atributos da classe Academia

--- src/br/com/zup/Academia.java
+++ src/br/com/zup/Academia.java
@@ -1,4 +1,9 @@
 package br.com.zup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Academia {
+    private List<Aula> aulas = new ArrayList<>();
+    private List<PersonalTrainer> personalTrainers = new ArrayList<>();
 }
