5088d597c4807d2c42c0666a43a723e93bb70f3a
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-09-30 16:38:23 -0300

    Criar um PersonalTrainer no sistema

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -8,4 +8,26 @@ public class Sistema {
         System.out.println(mensagem);
         return new Scanner(System.in);
     }
+
+
+    public static PersonalTrainer personal1(){
+        PersonalTrainer personal1 = new PersonalTrainer();
+        return personal1;
+    }
+
+    public static PersonalTrainer personal2(){
+        PersonalTrainer personal1 = new PersonalTrainer();
+        return personal1;
+    }
+
+    public static PersonalTrainer personal3(){
+        PersonalTrainer personal1 = new PersonalTrainer();
+        return personal1;
+    }
+
+    public static PersonalTrainer personal4(){
+        PersonalTrainer personal1 = new PersonalTrainer();
+        return personal1;
+    }
+
 }
