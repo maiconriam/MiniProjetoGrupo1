0f7a66ac39718720ef21797df1d46aca166ffe05
Maicon Riam de Oliveira <maiconrian00@gmail.com>
2021-09-30 14:55:15 -0300

    Criar metodo para PersonalTrainer

--- src/br/com/zup/PersonalTrainer.java
+++ src/br/com/zup/PersonalTrainer.java
@@ -1,4 +1,12 @@
 package br.com.zup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PersonalTrainer {
+    private int qtdDeAula;
+    private Aula aulaMinistrada;
+    private List<Aluno> alunos = new ArrayList<>();
+
+
 }
