bc0847a626f866de1563d22b767e3e4dcf8bd95f
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-30 15:05:43 -0300

    Criar Metodo Construtor Classe Aula

--- src/br/com/zup/Aula.java
+++ src/br/com/zup/Aula.java
@@ -7,4 +7,14 @@ public class Aula {
     private List<Horario> horario = new ArrayList<>();
     private String tipoDaAula;
     private double valorDaAula;
+
+    public Aula() {
+
+    }
+
+    public Aula(List<Horario> horario, String tipoDaAula, double valorDaAula) {
+        this.horario = horario;
+        this.tipoDaAula = tipoDaAula;
+        this.valorDaAula = valorDaAula;
+    }
 }
