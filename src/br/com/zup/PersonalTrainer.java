3fea25197e9e470ef8c5adb78695f6c03704dddc
Arê Nascimento <aretuza.gab@gmail.com>
2021-10-01 09:31:51 -0300

    Corrigir entrada do metodo

--- src/br/com/zup/PersonalTrainer.java
+++ src/br/com/zup/PersonalTrainer.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PersonalTrainer extends Pessoa {
-    private int qtdDeAula;
     private Aula aulaMinistrada;
     private List<Aluno> alunos = new ArrayList<>();
 
@@ -12,17 +11,9 @@ public class PersonalTrainer extends Pessoa {
 
     }
 
-    public PersonalTrainer(int qtdDeAula, Aula aulaMinistrada) {
-        this.qtdDeAula = qtdDeAula;
+    public PersonalTrainer(String nome, String cpf, Aula aulaMinistrada) {
+        super(nome, cpf);
         this.aulaMinistrada = aulaMinistrada;
-    }
-
-    public int getQtdDeAula() {
-        return qtdDeAula;
-    }
-
-    public void setQtdDeAula(int qtdDeAula) {
-        this.qtdDeAula = qtdDeAula;
     }
 
     public Aula getAulaMinistrada() {
@@ -37,18 +28,17 @@ public class PersonalTrainer extends Pessoa {
         return alunos;
     }
 
-    public void adicionarAluno(Aluno novoaluno){
+    public void adicionarAluno(Aluno novoaluno) {
         alunos.add(novoaluno);
     }
 
     @Override
     public String toString() {
         StringBuilder retorno = new StringBuilder();
-        retorno.append("Personal: " + getNome());
-        retorno.append("\nCPF do Personal: " + getCpf());
-        retorno.append("\nQuantidade de Aula: " + qtdDeAula);
-        retorno.append("\nAula Ministrada: " + aulaMinistrada.getTipoDaAula());
-        retorno.append("\nAlunos: " + alunos);
+        retorno.append("\n Personal: " + getNome());
+        retorno.append("\n CPF do Personal: " + getCpf());
+        retorno.append("\n Aula Ministrada: " + aulaMinistrada.getTipoDaAula());
+        retorno.append("\n Alunos: " + alunos);
         return retorno.toString();
     }
 }
