ef5b22fe441817b06f2b6fbba9554e4f5ba74f5b
Arê Nascimento <aretuza.gab@gmail.com>
2021-10-01 09:29:10 -0300

    Alterar metodo cadastrar aluno

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -1,65 +1,25 @@
 package br.com.zup;
 
-
 import java.util.ArrayList;
 import java.util.List;
-
 import java.util.Scanner;
 
 public class Sistema {
+    static List<PersonalTrainer> pt = new ArrayList<>();
 
     public static Scanner capturarDados(String mensagem) {
         System.out.println(mensagem);
         return new Scanner(System.in);
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     public static Aluno cadastrarAlunos() {
-        String aluno = capturarDados("Digite o nome do aluno que deseja matricular!").nextLine();
+        String nome = capturarDados("Digite o nome do aluno").nextLine();
+        String cpf = capturarDados("Digite o cpf do aluno").nextLine();
+        double altura = capturarDados("Digite a altura do aluno").nextDouble();
+        double peso = capturarDados("Digite o peso do aluno").nextDouble();
 
-        Aluno novoAluno = new Aluno();
-        novoAluno.setAluno(aluno);
+        Aluno novoAluno = new Aluno(nome, cpf, altura, peso);
         return novoAluno;
-    }
-
-    public static PersonalTrainer personal1(){
-        PersonalTrainer personal1 = new PersonalTrainer();
-        return personal1;
-    }
-
-    public static PersonalTrainer personal2(){
-        PersonalTrainer personal1 = new PersonalTrainer();
-        return personal1;
-    }
-
-    public static PersonalTrainer personal3(){
-        PersonalTrainer personal1 = new PersonalTrainer();
-        return personal1;
-    }
-
-    public static PersonalTrainer personal4(){
-        PersonalTrainer personal1 = new PersonalTrainer();
-        return personal1;
     }
 
 
